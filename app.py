1d94a0aec862741aac08b7d133b2a1ab0e4a1923
Sri Raghavan <sri.raghavan.1@gmail.com>
2013-01-19 11:50:58 -0500

    derpppp ajax parsing?

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
 import json
+import os
 import pydot
 from dot_parser import ParseException
 app = Flask(__name__, static_folder=".", template_folder=".", static_url_path="")
@@ -34,4 +35,5 @@ def parse():
 
 if __name__ == '__main__':
   app.debug = True
-  app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
