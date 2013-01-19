f9ba791e2891ef23c6454a083505f8c4180b1a1a
Dan Yang <dsyang92@gmail.com>
2013-01-19 05:23:47 -0500

    dummy app.py route

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__, static_folder=".", template_folder=".", static_url_path=""
 def index():
   return render_template('index.html')
 
+@app.route('/parse', methods=['POST'])
+def parse():
+  dotfile = request.form['dotfile']
+  return dotfile
+
+
 if __name__ == '__main__':
   app.run()
